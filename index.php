dba233f99626bf5ebcdc9e93dc0017dca90cdc86
alfredo89953 <134874235+alfredo89953@users.noreply.github.com>
2023-05-28 12:29:54 -0600

    index.php
    Calcular el salario de un trabajador con impuestos

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<body>
+
+<?php
+
+$SalarioTrabajador = 3500;
+
+$Impuesto = 20; // Porcentaje
+
+$SueldoReal = $SalarioTrabajador – (($SalarioTrabajador /
+
+100) * $Impuesto);
+
+echo “Sueldo del trabajador sin impuesto:  $SalarioTrabajador<BR>”;
+
+echo “Con el impuesto :$SueldoReal”;
+
+?>
+
+</body >
+
+</html>
